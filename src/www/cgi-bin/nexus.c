99c34e26a3ae95af2555f00bf9ff576beb599509
André Almeida <andralmeida0@gmail.com>
2018-09-08 11:12:49 +0100

    Compilation to linux system

--- src/www/cgi-bin/nexus.c
+++ src/www/cgi-bin/nexus.c
@@ -5,13 +5,14 @@
 #include <string.h>
 #ifdef WIN32
 #  include <winsock.h>
-//#  include <WS2tcpip.h>
+#  include <WS2tcpip.h>
 #  include <windows.h>
 #else
 #  include <sys/socket.h>
 #  include <sys/un.h>
+#  include <netinet/in.h>
 #endif
-	 
+
 #define SERVER  "/tmp/serversocket"
 #define MAXMSG  512
 
@@ -49,7 +50,9 @@ make_named_socket (const char *filename, int port)
   if (bind (sock, (struct sockaddr *) &name, sizeof(name)) < 0)
     {
       perror ("bind");
+#ifdef WIN32
       printf("%d\n", WSAGetLastError());
+#endif
       exit (EXIT_FAILURE);
     }
 
@@ -63,14 +66,17 @@ int main (void)
    struct sockaddr_in name;
    int size;
    int nbytes;
+#ifdef WIN32
    WORD wVersionRequested;
-    WSADATA wsaData; 
+    WSADATA wsaData;
+#endif
 
+#ifdef WIN32
    wVersionRequested = MAKEWORD(2, 2);
    WSAStartup(wVersionRequested, &wsaData);
    /* Remove the filename first, it's ok if the call fails */
    //unlink (SERVER);
-
+#endif
    /* Make the socket, then loop endlessly. */
    sock = make_named_socket (SERVER, 8888);
    while (1)
@@ -82,7 +88,7 @@ int main (void)
       {
          perror ("recfrom (server)");
          exit (EXIT_FAILURE);
-      } 
+      }
       /* Give a diagnostic message. */
       fprintf (stderr, "Server: got message: %s\n", message);
       /* Bounce the message back to the sender. */
@@ -93,5 +99,7 @@ int main (void)
          exit (EXIT_FAILURE);
       }
    }
+#ifdef WIN32
    WSACleanup();
+#endif
 }
