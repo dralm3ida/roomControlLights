78d509a2cc87174dbe496c4e58eeab4f9c0da73c
nfarinha <nuno.farinha@gmail.com>
2018-09-08 13:23:17 +0100

    Adição de processamento de mensagens recebidas do módulo de ultrasons e luz.

--- src/arduino/serial-comm/serial-comm.ino
+++ src/arduino/serial-comm/serial-comm.ino
@@ -2,14 +2,18 @@
 #include <EthernetUdp.h>
 #include "commands.h"
 
+#define DEBUG         Serial.print
+#define DEBUGLN       Serial.println
+
 #define PIN_LIGHTS_GROUP1   2
 #define PIN_LIGHTS_GROUP2   3
 
 unsigned char g_ultraSoundValues[] = {100, 20, 30, 48, 50, 68, 170, 89}; // 8 sensors, each one with range 0-256
 unsigned char g_lightSensor = 1;
 unsigned char g_pirSensor = 1;
-byte g_light_one_state = LOW;
-byte g_light_two_state = LOW;
+unsigned char g_light_one_state = LOW;
+unsigned char g_light_two_state = LOW;
+byte g_init = 0;
 
 byte mac[] = {
   0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xEE
@@ -38,17 +42,12 @@ void loop() {
   int packetSize = Udp.parsePacket();
   int incomingByte = 0;
 
-  if ( 0 == g_init )
-  {
-    //Udp.begin(portUltrasound);
-    g_init = 1;
-  }
-
-  Serial.print("serial ");
+  /*Serial.print("serial ");
   Serial.print(Serial.available());
   Serial.print(", ultrasound ");
   Serial.print(packetSize);
   Serial.println("");
+  */
   digitalWrite(LED_BUILTIN, LOW);
   if ( Serial.available() > 0 )
   {
@@ -107,9 +106,44 @@ void loop() {
   }
   else if ( (packetSize >= 8) )
   {
-    packetSize = Udp.read(g_ultraSoundValues, 8);
-    Serial.print("Contents: ");
-    Serial.println(packetSize);
+    byte  response[20] = {0};
+    int   i = 0;
+    
+    packetSize = Udp.read(response, 20);
+    switch ( response[0] )
+    {
+      case 'S':
+        Serial.print("Update sensors: ");
+        if ( packetSize >= 8 )
+        {
+          memcpy(g_ultraSoundValues, response, 8);
+          for ( i = 0; i < 8; ++i )
+          {
+            Serial.print(response[i], DEC);
+            Serial.print(" ");
+          }
+        }
+        if ( packetSize > 8 )
+        {
+          g_lightSensor = response[8];
+          Serial.print(g_lightSensor, DEC);
+        }
+        Serial.print("\n");
+        break;
+      case 'G':
+        IPAddress remoteAddr = Udp.remoteIP();
+        int       remotePort = Udp.remotePort();
+
+        DEBUG("{}GET: received request...");
+        response[0] = 9;
+        memcpy(response + 1, g_ultraSoundValues, 8);
+        response[9] = g_lightSensor;
+        Udp.beginPacket(Udp.remoteIP(), Udp.remotePort());
+        Udp.write(response, 10);
+        Udp.endPacket();
+        DEBUGLN("return 10 bytes");
+        break;
+    }
   }
   Serial.flush();
   delay(10);
--- src/www/cgi-bin/make2.sh
+++ src/www/cgi-bin/make2.sh
@@ -2,3 +2,5 @@
 echo "Compile nexus.c into a deamon to be used by apache"
 echo " + gcc.exe -o nexus.exe nexus.c -lwsock32"
 gcc.exe -o nexus.exe nexus.c -lwsock32
+echo " + gcc.exe -o sender.exe sender.c -lwsock32"
+gcc.exe -o sender.exe sender.c -lwsock32
